a88d140353bdffd089a2db4b9adfeaa416782c3b
Ran Schneider <rans@wix.com>
2018-06-19 15:20:03 +0300

    testing

--- build_modules.sh
+++ build_modules.sh
@@ -5,7 +5,7 @@
 NGINX_VERSION=${NGINX_VERSION:-1.13.7}
 GITHUB_MODULES=${GITHUB_MODULES:-openresty/headers-more-nginx-module nginx-modules/ngx_cache_purge opentracing-contrib/nginx-opentracing/opentracing }
 
-sudo apt-get install devscripts build-essential lintian
+apt-get install devscripts build-essential lintian
 
 for module in ${GITHUB_MODULES} ; do
     rm -rf /home/builduser/debuild
