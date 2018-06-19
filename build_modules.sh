6d28a1c17c136c241607b0ddf413bef6c8be1152
Ran Schneider <rans@wix.com>
2018-06-19 15:12:34 +0300

    testing

--- build_modules.sh
+++ build_modules.sh
@@ -5,7 +5,7 @@
 NGINX_VERSION=${NGINX_VERSION:-1.13.7}
 GITHUB_MODULES=${GITHUB_MODULES:-openresty/headers-more-nginx-module nginx-modules/ngx_cache_purge opentracing-contrib/nginx-opentracing/opentracing }
 
-dynamic_modules=""
+apt-get install debuild
 
 for module in ${GITHUB_MODULES} ; do
     rm -rf /home/builduser/debuild
