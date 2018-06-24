fd03c21bfe842ab1e14cda32172778c48ac6de1c
Ran Schneider <rans@wix.com>
2018-06-24 13:54:43 +0300

    changing nginx_version to nginx_plus_release

--- build_modules.sh
+++ build_modules.sh
@@ -2,10 +2,10 @@
 # You can specify nginx version and list of modules
 # via ENV variables
 
-NGINX_VERSION=${NGINX_VERSION:-1.13.7}
-GITHUB_MODULES=${GITHUB_MODULES:-openresty/headers-more-nginx-module nginx-modules/ngx_cache_purge opentracing-contrib/nginx-opentracing }
+NGINX_PLUS_RELEASE=${NGINX_PLUS_RELEASE:-15}
+GITHUB_MODULES=${GITHUB_MODULES:-openresty/headers-more-nginx-module nginx-modules/ngx_cache_purge opentracing-contrib/nginx-opentracing}
 
 
 for module in ${GITHUB_MODULES} ; do
-    ./build_module.sh -o /tmp -s -y -r 15 https://github.com/${module}.git
+    ./build_module.sh -o /tmp -s -y -r ${NGINX_PLUS_RELEASE} https://github.com/${module}.git
 done
