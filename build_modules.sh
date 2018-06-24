ca33b0ab4f5c81f15563242ea8cb008402385259
Ran Schneider <rans@wix.com>
2018-06-24 12:58:08 +0300

    updating build_modules.sh

--- build_modules.sh
+++ build_modules.sh
@@ -3,11 +3,9 @@
 # via ENV variables
 
 NGINX_VERSION=${NGINX_VERSION:-1.13.7}
-GITHUB_MODULES=${GITHUB_MODULES:-openresty/headers-more-nginx-module nginx-modules/ngx_cache_purge opentracing-contrib/nginx-opentracing/opentracing }
+GITHUB_MODULES=${GITHUB_MODULES:-openresty/headers-more-nginx-module nginx-modules/ngx_cache_purge opentracing-contrib/nginx-opentracing }
 
-apt-get install devscripts build-essential lintian
 
 for module in ${GITHUB_MODULES} ; do
-    rm -rf /home/builduser/debuild
-    ./build_module.sh -s -r 15 https://github.com/$(echo $module | sed 's#^\([^/]*\)/\([^/]*\)\(/[^/]*\)$#\1/\2#').git
+    ./build_module.sh -o /tmp -s -y -r 15 https://github.com/${module}.git
 done
