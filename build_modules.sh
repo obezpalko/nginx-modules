f5da50f4236c755911739f6769eb3d306c7e59c1
Ran Schneider <rans@wix.com>
2018-07-01 16:08:21 +0300

    setting variable for output directory

--- build_modules.sh
+++ build_modules.sh
@@ -4,15 +4,20 @@
 
 NGINX_PLUS_RELEASE=${NGINX_PLUS_RELEASE:-15}
 GITHUB_MODULES=${GITHUB_MODULES:-openresty/headers-more-nginx-module nginx-modules/ngx_cache_purge opentracing-contrib/nginx-opentracing}
-rm -rf /tmp/nginx_modules_packages
-mkdir -p /tmp/nginx_modules_packages
+ARTIFACTS_DIR=/tmp/nginx_modules_packages
+
+
+mkdir -p ${ARTIFACTS_DIR}
 
 for module in ${GITHUB_MODULES} ; do
-    ./build_module.sh -o /tmp/nginx_modules_packages -s -y -r ${NGINX_PLUS_RELEASE} https://github.com/${module}.git
+	./build_module.sh -o ${ARTIFACTS_DIR} -s -y -r ${NGINX_PLUS_RELEASE} https://github.com/${module}.git
 done
 
-#uploading to artifactory
-for package in $(ls /tmp/nginx_modules_packages) ; do
-    curl -u "admin:avIIon11" -XPUT "https://repo.dev.wixpress.com/artifactory/nginx-modules/${package};deb.distribution=stable;deb.component=main;deb.architecture=amd64" -T /tmp/nginx_modules_packages/${package}
+
+## Uploading packages to Artifactory
+for package in $(ls ${ARTIFACTS_DIR}) ; do
+    curl -u "admin:avIIon11" -XPUT "https://repo.dev.wixpress.com/artifactory/nginx-modules/${package};deb.distribution=stable;deb.component=main;deb.architecture=amd64" -T ${ARTIFACTS_DIR}/${package}
 done
+
+rm -rf ${ARTIFACTS_DIR}
 
